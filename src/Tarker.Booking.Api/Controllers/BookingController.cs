c023007461d683364da66ce9edb8ac1d06c0e2f5
Luisdanielmartinez <luisdanielcurso@gmail.com>
2025-02-10 22:24:16 -0400

    added methods on booking and custumer controllers

--- src/Tarker.Booking.Api/Controllers/BookingController.cs
+++ src/Tarker.Booking.Api/Controllers/BookingController.cs
@@ -1,15 +1,62 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Tarker.Booking.Application.DataBase.Booking.Commands.CreateBooking;
+using Tarker.Booking.Application.DataBase.Booking.Queries.GetAllBooking;
+using Tarker.Booking.Application.DataBase.Booking.Queries.GetBookingByType;
+using Tarker.Booking.Application.DataBase.Booking.Queries.GetBookingsByDocumentNumber;
+using Tarker.Booking.Application.DataBase.User.Commands.CreateUser;
+using Tarker.Booking.Application.DataBase.User.Queries.GetAllUser;
+using Tarker.Booking.Application.DataBase.User.Queries.GetUserById;
+using Tarker.Booking.Application.Exceptions;
+using Tarker.Booking.Application.Features;
 
 namespace Tarker.Booking.Api.Controllers
 {
     [Route("api/v1/[controller]")]
     [ApiController]
+    [TypeFilter(typeof(ExceptionManager))]
     public class BookingController : ControllerBase
     {
-        public BookingController()
+        [HttpPost("create")]
+        public async Task<IActionResult> Create([FromBody] CreateBookingModel model, [FromServices] ICreateBookingCommand createBookingCommand)
         {
-                
+            var data = await createBookingCommand.Execute(model);
+
+            return StatusCode(StatusCodes.Status201Created,
+                ResponseApiService.Response(StatusCodes.Status201Created, data));
+        }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAll([FromServices] IGetAllBookingQuery getAllBookingQuery)
+        {
+            var data = await getAllBookingQuery.Execute();
+
+            if (data == null || data.Count == 0)
+                return StatusCode(StatusCodes.Status404NotFound, ResponseApiService.Response(StatusCodes.Status404NotFound));
+
+            return StatusCode(StatusCodes.Status200OK, ResponseApiService.Response(StatusCodes.Status200OK, data));
+        }
+
+        [HttpGet("get-by-documentNumber/{documentNumber}")]
+        public async Task<IActionResult> GetById(string documentNumber, [FromServices]  IGetBookingsByDocumentNumberQuery getBookingsByDocumentNumberQuery)
+        {
+            var data = await getBookingsByDocumentNumberQuery.Execute(documentNumber);
+
+            if (data == null)
+                return StatusCode(StatusCodes.Status404NotFound, ResponseApiService.Response(StatusCodes.Status404NotFound));
+
+            return StatusCode(StatusCodes.Status200OK, ResponseApiService.Response(StatusCodes.Status200OK, data));
+        }
+
+        [HttpGet("get-by-type/{type}")]
+        public async Task<IActionResult> GetById(string type, [FromServices] IGetBookingByTypeQuery getBookingByTypeQuery)
+        {
+            var data = await getBookingByTypeQuery.Execute(type);
+
+            if (data == null)
+                return StatusCode(StatusCodes.Status404NotFound, ResponseApiService.Response(StatusCodes.Status404NotFound));
+
+            return StatusCode(StatusCodes.Status200OK, ResponseApiService.Response(StatusCodes.Status200OK, data));
         }
     }
 }
--- src/Tarker.Booking.Application/DataBase/Booking/Commands/CreateBooking/CreateBookingModel.cs
+++ src/Tarker.Booking.Application/DataBase/Booking/Commands/CreateBooking/CreateBookingModel.cs
@@ -9,7 +9,6 @@ namespace Tarker.Booking.Application.DataBase.Booking.Commands.CreateBooking
     public class CreateBookingModel
     {
         public int BookingId { get; set; }
-        public DateTime RegisterDate { get; set; }
         public string Code { get; set; }
         public string Type { get; set; }
         public int CustomerId { get; set; }
