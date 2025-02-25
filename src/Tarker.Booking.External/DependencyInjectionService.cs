e8cf6096cca8bb3998b319256df3596457469ffb
Luisdanielmartinez <luisdanielcurso@gmail.com>
2025-02-24 21:31:54 -0400

    added services to get toke jwt

--- src/Tarker.Booking.External/DependencyInjectionService.cs
+++ src/Tarker.Booking.External/DependencyInjectionService.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using FluentValidation;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Tarker.Booking.Application.DataBase.Booking.Commands.CreateBooking;
+using Tarker.Booking.Application.Extermal.GetTokenJwt;
+using Tarker.Booking.Application.Validators.Booking;
+using Tarker.Booking.External.GetTokenJwt;
 
 namespace Tarker.Booking.External
 {
@@ -7,6 +12,7 @@ namespace Tarker.Booking.External
     {
         public static IServiceCollection AddExternal(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddSingleton<IGetTokenJwtService, GetTokenJwtService>();
             return services;
         }
     }
